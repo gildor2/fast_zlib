f7d0a9b0822effc42624734367c28e064b6df137
Konstantin Nosov <git@gildor.org>
2017-02-09 16:23:48 +0300

    New test application.

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,36 +1,149 @@
-#include <windows.h>
 #include <stdio.h>
+#include <time.h>
+
+// includes for file enumeration
+#if _WIN32
+#	include <io.h>					// for findfirst() set
+#else
+#	include <dirent.h>				// for opendir() etc
+#	include <sys/stat.h>			// for stat()
+#endif
+
+#include <vector>
+#include <string>
+
 #include "zlib/zlib.h"
 
-//#pragma comment(lib, "lib/Release/zlib.lib")
+#define STR2(s) #s
+#define STR(s) STR2(s)
 
+#define BUFFER_SIZE		(256<<20)
+#define MAX_ITERATIONS	1			// number of passes to fully fill buffer, i.e. total processed data size will be up to (BUFFER_SIZE * MAX_ITERATIONS)
+#define COMPRESS_LEVEL	9
 
-void main (int argc, const char **argv)
+std::vector<std::string> fileList;
+
+static bool ScanDirectory(const char *dir, bool recurse = true)
 {
-	FILE *f;
-	gzFile gz;
-	char buf[8192];
-
-	if (!(f = fopen("uncompressed.txt", "rb")))
+	char Path[1024];
+	bool res = true;
+//	printf("Scan %s\n", dir);
+#if _WIN32
+	sprintf(Path, "%s/*.*", dir);
+	_finddatai64_t found;
+	intptr_t hFind = _findfirsti64(Path, &found);
+	if (hFind == -1) return true;
+	do
 	{
-		puts("!open");
-		exit(1);
+		if (found.name[0] == '.') continue;			// "." or ".."
+		sprintf(Path, "%s/%s", dir, found.name);
+		// directory -> recurse
+		if (found.attrib & _A_SUBDIR)
+		{
+			if (recurse)
+				res = ScanDirectory(Path, recurse);
+			else
+				res = true;
+		}
+		else
+		{
+			fileList.push_back(Path);
+			res = true;
+		}
+	} while (res && _findnexti64(hFind, &found) != -1);
+	_findclose(hFind);
+#else
+	DIR *find = opendir(dir);
+	if (!find) return true;
+	struct dirent *ent;
+	while (/*res &&*/ (ent = readdir(find)))
+	{
+		if (ent->d_name[0] == '.') continue;			// "." or ".."
+		sprintf(Path, "%s/%s", dir, ent->d_name);
+		// directory -> recurse
+		// note: using 'stat64' here because 'stat' ignores large files
+		struct stat64 buf;
+		if (stat64(Path, &buf) < 0) continue;			// or break?
+		if (S_ISDIR(buf.st_mode))
+		{
+			if (recurse)
+				res = ScanDirectory(Path, recurse);
+			else
+				res = true;
+		}
+		else
+		{
+			fileList.push_back(Path);
+			res = true;
+		}
+	}
+	closedir(find);
+#endif
+	return res;
+}
+
+static unsigned char buffer[BUFFER_SIZE];
+static int bytesInBuffer = 0;
+static int currentFile = 0;
+
+static bool FillBuffer()
+{
+	bytesInBuffer = 0;
+
+	while (bytesInBuffer < BUFFER_SIZE)
+	{
+		if (currentFile >= fileList.size()) break;
+		const char* filename = fileList[currentFile++].c_str();
+		FILE* f = fopen(filename, "rb");
+		if (!f) continue; // no error, skip this file
+
+		int bytesRead = fread(buffer + bytesInBuffer, 1, BUFFER_SIZE - bytesInBuffer, f);
+		fclose(f);
+		bytesInBuffer += bytesRead;
+//		printf("file %s %d bytes\n", filename, bytesRead);
+	}
+//	printf("... %d bytes\n", bytesInBuffer);
+
+	return (bytesInBuffer > 0);
+}
+
+
+
+int main(int argc, const char **argv)
+{
+	if (argc <= 1)
+	{
+		printf("Usage: test <directory>\n");
+		return 1;
 	}
 
-	int t = GetTickCount();
-	gz = gzopen("compressed2.gz", "wb9");
-	while (!feof(f))
+	ScanDirectory(argv[1]);
+//	printf("%d files\n", fileList.size());
+
+	clock_t clocks = 0;
+
+	const char* compressedFile = "compressed-" STR(VERSION) ".gz";
+	gzFile gz = gzopen(compressedFile, "wb" STR(COMPRESS_LEVEL));
+	int iteration = 0;
+	int totalDataSize = 0;
+	while (FillBuffer() && iteration < MAX_ITERATIONS)
 	{
-		int len = fread(buf, 1, sizeof(buf), f);
-		gzwrite(gz, buf, len);
+		clock_t clock_a = clock();
+		gzwrite(gz, buffer, bytesInBuffer);
+		clocks += clock() - clock_a;
+		iteration++;
+		totalDataSize += bytesInBuffer;
 	}
-	fclose(f);
 	gzclose(gz);
 
-	printf("seconds:%d/10\n", (GetTickCount()-t) / 100);
-	f = fopen("compressed2.gz", "rb");
+	FILE* f = fopen(compressedFile, "rb");
 	fseek(f, 0, SEEK_END);
-	int len = ftell(f);
+	int compressedSize = ftell(f);
 	fclose(f);
-	printf("size:%d\n", len);
+
+	printf("Compressed %.1f Mb of data by method %s with level %d (%s)\n", (float)totalDataSize / (1024*1024), STR(VERSION), COMPRESS_LEVEL, argv[1]);
+	printf("Time: %.1f s\n", clocks / (float)CLOCKS_PER_SEC);
+	printf("Size: %d\n", compressedSize);
+
+	return 0;
 }
