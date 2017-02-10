bccce0dc439b3355c03b22b0e7b26de84e24acd1
Konstantin Nosov <git@gildor.org>
2017-02-10 19:06:07 +0300

    Added test launcher

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# build all targets with hiding build output
+./build.sh > /dev/null 2>&1
+
+function DoTests
+{
+	local dir=$1
+	obj/bin/test-Asm-win32 $dir
+	obj/bin/test-C-win32 $dir
+	obj/bin/test-Orig-win32 $dir
+}
+
+DoTests C:/Projects/Epic/UnrealEngine4-latest/Engine/Source/Runtime
+DoTests C:/3-UnrealEngine/4.14/Engine/Binaries/Win64
+DoTests C:/1
